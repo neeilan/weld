8f5bcb9ebace8c86dc692455310507c9f4a76cd0
neeilan <neeilan@outlook.com>
2023-02-07 00:49:22 -0500

    [elf_parser] Read section headers + names

--- elf_parser/src/lib.rs
+++ elf_parser/src/lib.rs
@@ -1,8 +1,7 @@
-
+use std::ffi::CStr;
 use std::vec::Vec;
 
-
-pub fn parse(bytes : Vec<u8>) -> elf::File {
+pub fn parse(bytes: Vec<u8>) -> elf::File {
     println!("{}-byte ELF file", bytes.len());
 
     let mut file = elf::File::default();
@@ -10,22 +9,38 @@ pub fn parse(bytes : Vec<u8>) -> elf::File {
     let file_hdr_size = std::mem::size_of::<elf::FileHeader>();
     let hdr_bytes = &bytes[..file_hdr_size];
     file.file_header = unsafe { std::ptr::read(hdr_bytes.as_ptr() as *const _) };
-    println!("{:?}", file.file_header);
+
+    // Parse the section header table
+    let mut section_headers = Vec::new();
+    for i in 0..file.file_header.section_header_entry_count {
+        let shdr_base: usize = (file.file_header.section_header_offset as usize)
+            + (i as usize) * std::mem::size_of::<elf::SectionHeader>();
+        let shdr_end: usize = shdr_base + std::mem::size_of::<elf::SectionHeader>();
+        let shdr_bytes = &bytes[shdr_base..shdr_end];
+
+        let shdr: elf::SectionHeader = unsafe { std::ptr::read(shdr_bytes.as_ptr() as *const _) };
+        section_headers.push(shdr);
+    }
+
+    let section_name_string_table_offset = section_headers
+        [file.file_header.sh_section_name_stringtab_entry_index as usize]
+        .offset as usize;
+
+    for i in 0..file.file_header.section_header_entry_count {
+        let shdr = &section_headers[i as usize];
+        unsafe {
+            let section_name_start_offset = section_name_string_table_offset + (shdr.name as usize);
+            let str_ref = &bytes[section_name_start_offset..];
+
+            println!(
+                "    > Read section header [{:?}] : {:?}",
+                shdr.section_type,
+                CStr::from_ptr(str_ref.as_ptr() as *const _)
+                    .to_str()
+                    .unwrap()
+            );
+        }
+    }
 
     return file;
-}
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
